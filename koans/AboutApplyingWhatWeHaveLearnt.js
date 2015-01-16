4c8b440d663876d4f290adb5d43a6af6d92a447b
jnis77diver <jsnisenson@gmail.com>
2015-01-16 16:08:54 -0500

    fix AboutFunctions.js, AboutHigherOrderFucntions.js, AboutInheritance.js, AboutMutability.js. AboutApplyingWhatWeHaveLearnt still has errors

--- koans/AboutApplyingWhatWeHaveLearnt.js
+++ koans/AboutApplyingWhatWeHaveLearnt.js
@@ -32,7 +32,7 @@ describe("About Applying What We Have Learnt", function() {
         }
     }
 
-    expect(productsICanEat.length).toBe(FILL_ME_IN);
+    expect(productsICanEat.length).toBe(1);
   });
 
   it("given I'm allergic to nuts and hate mushrooms, it should find a pizza I can eat (functional)", function () {
@@ -40,8 +40,12 @@ describe("About Applying What We Have Learnt", function() {
       var productsICanEat = [];
 
       /* solve using filter() & all() / any() */
+      _(products).filter(function(item) { return item.containsNuts === false; }).chain()
+                        .filter(function(x){ return x.ingredients.all(function(ingredient) { 
+                        return ingredient !== "mushrooms"; }); });
+                      
 
-      expect(productsICanEat.length).toBe(FILL_ME_IN);
+      expect(productsICanEat.length).toBe(1);
   });
 
   /*********************************************************************************/
